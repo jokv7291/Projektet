7ffab05ed3b7113d7d8fd20ceeba5624106b5098
Josef Kvist <josef@timetailor.se>
2011-11-04 14:30:31 +0100

    added db

--- protected/config/console.php
+++ protected/config/console.php
@@ -11,14 +11,14 @@ return array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=projektet',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'root',
 			'charset' => 'utf8',
 		),
-		*/
+		
 	),
 );
