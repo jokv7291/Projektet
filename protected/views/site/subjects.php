c928511004abc7f8d0aeba7d3f630c326669b727
Josef Kvist <josef@timetailor.se>
2011-11-09 16:46:25 +0100

    subject gridview

--- protected/views/site/subjects.php
+++ protected/views/site/subjects.php
@@ -41,7 +41,17 @@
 
 <h1>Ämnen</h1>
 
-<?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
+<?php 
+
+$this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'user-grid',
+	'dataProvider'=>$model->search(),
+	//'filter'=>$model,
+	'columns'=>array(
+		'subject_short',
+		'subject_name',
+		array(
+			'class'=>'CButtonColumn',	
+			),
+	),
 )); ?>
