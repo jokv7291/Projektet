7cb994a1d8bdcaf17e13730db7d017a197cf0caa
Josef Kvist <josef@timetailor.se>
2011-11-09 15:26:44 +0100

    subjects 0.2

---
+++ protected/views/site/_view.php
@@ -0,0 +1,11 @@
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('subject_name')); ?>:</b>
+	<?php echo CHtml::encode($data->subject_name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('subject_short')); ?>:</b>
+	<?php echo CHtml::encode($data->subject_short); ?>
+	<br />
+
+</div>
--- protected/views/site/subjects.php
+++ protected/views/site/subjects.php
@@ -1,3 +1,12 @@
+<?php $this->breadcrumbs=array(
+	'Subjects'=>array('subjects'),
+	'Groups',
+	'step3',
+	'osv',
+);
+
+?>
+
 <div class="form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
@@ -11,13 +20,13 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'subject_name'); ?>
-		<?php echo $form->textField($model,'subject_name'); ?>
+		<?php echo $form->textField($model,'subject_name', array('value'=>'')); ?>
 		<?php echo $form->error($model,'subject_name'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'subject_short'); ?>
-		<?php echo $form->textField($model,'subject_short'); ?>
+		<?php echo $form->textField($model,'subject_short', array('value'=>'')); ?>
 		<?php echo $form->error($model,'subject_short'); ?>
 	</div>
 
@@ -30,7 +39,7 @@
 
 </div><!-- form -->
 
-<h1>Subjects</h1>
+<h1>Ämnen</h1>
 
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
